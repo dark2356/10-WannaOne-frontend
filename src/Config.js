febb06c2fac58cda9e3b74749141d4be7e1e7053
junggyoo <jungggyu.1@gmail.com>
2020-08-04 21:50:16 +0900

    Modify: 코드 리뷰 반영 및 충돌 해결

--- src/Config.js
+++ src/Config.js
@@ -1,1 +1,5 @@
 export const API_URL = "http://10.58.1.236:8000";
+
+export const SIGNUP_API_URL = "http://10.58.0.30:8000/user/sign-up"
+
+export const KAKAO_LOGIN_API_URL = "http://10.58.7.58:8000/user/sign-in/kakao"
